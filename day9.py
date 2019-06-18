dc4b7ac822c1426cbb2728aea0a212a0d58bfca9
rashi53 <51444233+rashi53@users.noreply.github.com>
2019-06-18 17:57:45 +0530

    Add files via upload

---
+++ day9.py
@@ -0,0 +1,147 @@
+Python 3.7.3 (v3.7.3:ef4ec6ed12, Mar 25 2019, 21:26:53) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+================== RESTART: C:/python/Python37-32/day9/1.py ==================
+4
+ginger
+ginger,4
+brown
+3
+brown,3
+>>> 
+================== RESTART: C:/python/Python37-32/day9/2.py ==================
+wool
+tommy has 400 price and its color is  brown
+wool
+shern has 400 price and its color is  white
+400
+400
+400
+wool
+abc has 400 price and its color is  blue
+>>> 
+================== RESTART: C:/python/Python37-32/day9/2.py ==================
+wool
+tommy has 400 price and its color is  brown
+wool
+shern has 400 price and its color is  white
+400
+400
+Traceback (most recent call last):
+  File "C:/python/Python37-32/day9/2.py", line 16, in <module>
+    print(pet1.x)
+AttributeError: 'Dog' object has no attribute 'x'
+>>> 
+================== RESTART: C:/python/Python37-32/day9/3.py ==================
+Grr
+wool
+tommy is of color brown
+>>> 
+================== RESTART: C:/python/Python37-32/day9/4.py ==================
+cat meows
+lucy
+>>> 
+================== RESTART: C:/python/Python37-32/day9/4.py ==================
+Traceback (most recent call last):
+  File "C:/python/Python37-32/day9/4.py", line 14, in <module>
+    pet1.bark()
+AttributeError: 'Dog' object has no attribute 'bark'
+>>> 
+================== RESTART: C:/python/Python37-32/day9/4.py ==================
+woof
+cat meows
+tomy
+lucy
+>>> 
+================== RESTART: C:/python/Python37-32/day9/5.py ==================
+wool
+wool
+wool
+>>> 
+================== RESTART: C:/python/Python37-32/day9/5.py ==================
+woof
+woof
+woof
+>>> 
+================== RESTART: C:/python/Python37-32/day9/5.py ==================
+woof
+Grr
+woof
+>>> 
+================== RESTART: C:/python/Python37-32/day9/6.py ==================
+method of class A..
+method of class B..
+method of class C..
+>>> 
+================== RESTART: C:/python/Python37-32/day9/7.py ==================
+method of class A..
+method of class B..
+method of class C..
+>>> 
+================== RESTART: C:/python/Python37-32/day9/8.py ==================
+method of class A..
+Traceback (most recent call last):
+  File "C:/python/Python37-32/day9/8.py", line 14, in <module>
+    ob.second_method()
+AttributeError: 'C' object has no attribute 'second_method'
+>>> 
+================== RESTART: C:/python/Python37-32/day9/8.py ==================
+method of class A..
+method of class C..
+>>> 
+================== RESTART: C:/python/Python37-32/day9/8.py ==================
+method of class B..
+method of class C..
+>>> 
+================== RESTART: C:/python/Python37-32/day9/9.py ==================
+method of class B..
+method of class A..
+>>> 
+================= RESTART: C:/python/Python37-32/day9/10.py =================
+method of class C2..
+method of class B..
+>>> 
+================= RESTART: C:/python/Python37-32/day9/11.py =================
+8
+15
+8
+15
+>>> 
+================= RESTART: C:/python/Python37-32/day9/11.py =================
+8
+15
+2
+-1
+>>> 
+================= RESTART: C:/python/Python37-32/day9/11.py =================
+8
+15
+2
+-1
+2+-1i
+>>> 
+================= RESTART: C:/python/Python37-32/day9/11.py =================
+8
+15
+8+15i
+2
+-1
+2+-1i
+>>> 
+================= RESTART: C:/python/Python37-32/day9/12.py =================
+total Employee  1
+Name; mahima ,salary: 55000
+Name; Abhinn ,salary: 54000
+total Employee  2
+total employee 3
+total employee 3
+total employee 3
+total Employee  3
+>>> 
+================= RESTART: C:/python/Python37-32/day9/13.py =================
+Name; nikhil ,salary: 9999
+Name; nikhil ,salary: 17000
+Name; manoj ,salary: 17000
+3
+Name; manoj ,salary: 17000
+>>> 
